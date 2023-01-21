7256f53e563b3be5dc2effcc0baa8ec5c4ec70a2
sujit bista <sujitbista@sujits-MacBook-Pro.local>
2023-01-21 20:16:34 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState } from "react";
+import {useLocation,BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import Login from "./components/login";
+import Admin from "./components/admin";
 
 function App() {
+  let isAuthenticated = false;
+  if(isAuthenticated) {
+     isAuthenticated = isAuthenticated;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+      <Router>
+      <Routes>
+        <Route exact path="/" element={<Login/>}/>
+        {isAuthenticated} && <Route exact path="/admin" element={ <Admin /> } /> 
+      </Routes>
+      </Router>
+      </>
   );
 }
-
+  
 export default App;
